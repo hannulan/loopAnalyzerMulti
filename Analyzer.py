Updated to get bolus and basal dataframes correct. Added calculation of totalt daily dose. Need to fix data so only whole 24 hours periods are included and calculate num of 24 periods, to get correct calculation for this. No unit tests added yet for new added functions

Analyzer.py
@@ -28,6 +28,8 @@ class Analyzer:
     rangeHyperLevel1 = [18*x for x in [10, 13.9]] #[10*18,13.9*18];
     rangeHyperLevel2 = [rangeHyperLevel1[1]];
     
+    timeCGMStableMin = 20;
+    timeCGMStableSec = timeCGMStableMin*60;
     #
     #  hypoL2  |  hypoL1    |    range         |  hyperL1    |  hyperL2
     # ---------|------------|-----------|------|-------------|------------- 
@@ -78,9 +80,12 @@ class Analyzer:
     tdd = 0; 
     
     
-    def __init__(self, patientName, dfCGM):
+    def __init__(self, patientName, numDayNight, dfCGM, dfInsulin):
         self.patientName = patientName;
-        self.dfCGM = dfCGM; 
+        self.numDayNight = numDayNight; 
+        self.dfCGM       = dfCGM; 
+        self.dfInsulin   = dfInsulin; 
+  
         # Calc idx night and day
         self.idxNight, self.idxDay, self.idxDay2 = self.findIdxNightDay();
         self.dfCGMNight = dfCGM.iloc[self.idxNight];
@@ -88,8 +93,6 @@ class Analyzer:
         self.dfCGMDay2  = dfCGM.iloc[self.idxDay2];
   
     def calcAllCGM(self):
-        
-        
         self.tir = self.calcTimeInXNew(self.dfCGM, self.tirLevel, '[]')  # [3.9 10]
         self.tit = self.calcTimeInXNew(self.dfCGM, self.titLevel, '[]') # [4 8]
         self.tihyperLevel1Value = self.calcTimeInXNew(self.dfCGM, self.rangeHyperLevel1, '[]');                                                # ]10 13.9] 
@@ -117,10 +120,11 @@ class Analyzer:
         self.cgmPGS  = 0; 
         self.cgmMAGE = 0; 
         
-        #cgmMean, cgmSD, cgmSCV = self.calcCGMVariation(self.dfCGMDay);
-        #self.cgmSDDay  = cgmSD; 
-        #cgmMean, cgmSD, cgmSCV = self.calcCGMVariation(self.dfCGMNight);
-        #self.cgmSDNight = cgmSD; 
+        # Calculate cgm values for day and nights
+        cgmMean, cgmSD, cgmSCV = self.calcCGMVariation(self.dfCGMDay);
+        self.cgmSDDay  = cgmSD; 
+        cgmMean, cgmSD, cgmSCV = self.calcCGMVariation(self.dfCGMNight);
+        self.cgmSDNight = cgmSD; 
 
         return 1; 
     
@@ -152,7 +156,14 @@ class Analyzer:
         return idxNight, idxDay, idxDay2    
    
     def calcAllInsulin(self):
-        self.tdd = 0; 
+        sumBolus  = np.sum(self.dfInsulin['bolus']); 
+        basal     = self.dfInsulin['rate'];
+        timeHours = self.dfInsulin['deltaTimeSec']/60/60;
+        sumBasal  = np.sum(basal*timeHours); 
+        
+        self.tdd = round(sumBasal/self.numDayNight, 4); 
+        
+        return 1;  
         
     def writeAll(self):
         ## Write result in text format to a file:     
@@ -183,7 +194,7 @@ class Analyzer:
         
         file_object.write('tirDag;  ' + str(self.tirDag) + ';\n');
         file_object.write('tirNatt; ' + str(self.tirNatt) + ';\n');
-        file_object.write('tdd; ' + str(self.tdd) + ';\n');
+        file_object.write('TDD genomsnitt; ' + str(self.tdd) + ';\n');
         file_object.close()
 
 # PGS
@@ -212,11 +223,12 @@ class Analyzer:
     def calcTimeInXNew(self, dfCGM, xRange, mode):
         # This functions calculation percentage of time in range. 
         # TODO: Remove time slots that are larger than 5 minutes
+        # TODO: Remove some self. which is there for debugging
         # idxInRange    = (dfCGM['cgm'] <=  xRange[1]) & (dfCGM['cgm'] >=  xRange[0])
         
         if not dfCGM.empty: #operator.not_(dfCGM.empty):
             if mode == '[]':
-                idxInRange    = (xRange[0] <= dfCGM['cgm']) & (dfCGM['cgm'] <=  xRange[1]); 
+                idxInRange    = (xRange[0] <= dfCGM['cgm']) & (dfCGM['cgm'] <=  xRange[1]) 
             elif mode == ']]':
                 idxInRange    = (xRange[0] <  dfCGM['cgm']) & (dfCGM['cgm'] <=  xRange[1]) 
             elif mode == '...[':
@@ -225,8 +237,36 @@ class Analyzer:
                 idxInRange    = (dfCGM['cgm'] > xRange[0]) 
             else:
                 idxInRange = [];
-            totTime = sum(dfCGM['deltaTime'][1:len(dfCGM)])
-            tix    = sum(dfCGM['deltaTime'][1:len(dfCGM)][idxInRange])/totTime;
+            
+            #### Här är kod som ska limitera tiden i vektorn timeTemp till timeCGMStableSec (20*60 sekunder just nu)
+            #### Det här funkar inte riktigt för alla fall utanjag får fel på rad 104 när jag anropar detta.
+            #### Rad 104: self.tihypoNightLevel1Value = self.calcTimeInXNew(self.dfCGMNight, self.rangeHypoLevel1, '[]'); # [3.0 3.8]
+            #### Felet beror nog på att nåt idx vektor är tom att koden inte tar hänsyn till detta. 
+                
+            #totTime  = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)])
+            self.timeTempAll = self.dfCGM['deltaTimeSec'];
+            self.timeTempAll.iloc[0] = 0; # Set deltaTime for the first (last) sample to zero since it is unknown. 
+            self.timeTempAll = np.minimum(self.timeTempAll, self.timeCGMStableSec)
+            
+            #self.timeTempInRange =  self.dfCGM['deltaTimeSec'][idxInRange]; #Denna rad funkar inte
+            self.timeTempInRange = self.dfCGM['deltaTimeSec']; ## Denna rad är en snabbfix för att koden inte ska krascha
+            self.timeTempInRange.iloc[0] = 0; # Set deltaTime for the first (last) sample to zero since it is unknown. 
+            self.timeTempInRange = np.minimum(self.timeTempInRange, self.timeCGMStableSec)
+        
+            #
+            self.totTime = sum(self.timeTempAll);
+            self.inTime  = sum(self.timeTempInRange);
+            tix = self.inTime/self.totTime;
+            
+            self.idxInRange = idxInRange; 
+            ########################################
+            #### Här slutar den koden. ####
+            #########################################
+            
+            
+            
+            totTime  = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)])
+            tix    = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)][idxInRange])/totTime;
             tix = round(tix, 4); 
         else:
             tix = 0; 
@@ -245,18 +285,18 @@ class Analyzer:
         idxBelowRange = dfCGM['cgm'] < xRange[0];
         idxInRange    = (dfCGM['cgm'] <=  xRange[1]) & (dfCGM['cgm'] >=  xRange[0])
         
-        totTime = sum(dfCGM['deltaTime'][1:len(dfCGM)])
+        totTime = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)])
         
-        timeAboveRange_per = sum(dfCGM['deltaTime'][1:len(dfCGM)][idxAboveRange])/totTime;
-        timeBelowRange_per = sum(dfCGM['deltaTime'][1:len(dfCGM)][idxBelowRange])/totTime;
-        timeInRange_per    = sum(dfCGM['deltaTime'][1:len(dfCGM)][idxInRange])/totTime;
+        timeAboveRange_per = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)][idxAboveRange])/totTime;
+        timeBelowRange_per = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)][idxBelowRange])/totTime;
+        timeInRange_per    = sum(dfCGM['deltaTimeSec'][1:len(dfCGM)][idxInRange])/totTime;
         return timeAboveRange_per, timeBelowRange_per, timeInRange_per
     def basalBolusPercentage(self, dfBasal, dfBolus):
         # Thie function...
         
         totBolus = sum(dfBolus['bolus']);
         N = len(dfBasal);
-        totBasal = sum(dfBasal['basalRate'][1:N]*dfBasal['deltaTime'][1:N]/3600);
+        totBasal = sum(dfBasal['basalRate'][1:N]*dfBasal['deltaTimeSec'][1:N]/3600);
         tot = totBolus + totBasal; 
         basalPercentage = totBasal/tot; 
         bolusPercentage = totBolus/tot; 

Reader.py
@@ -27,20 +27,25 @@ class Reader:
         self.dfEntries = pd.DataFrame();
         self.dfTreatments = pd.DataFrame();
         
-        self.dfCGM   = pd.DataFrame(columns=['dateTime', 'cgm', 'deltaTime']);
+        self.dfCGM   = pd.DataFrame(columns=['dateTime', 'cgm', 'deltaTimeSec']);
         self.dfBolus = pd.DataFrame(columns=['dateTime', 'bolus'])
         self.dfCarbs = pd.DataFrame(columns=['dateTime', 'carbs'])
-        self.dfBasal = pd.DataFrame(columns=['dateTime', 'basalRate', 'deltaTime'])
+        self.dfBasal = pd.DataFrame(columns=['dateTime', 'basalRate', 'deltaTimeSec'])
+        
         self.readData();
+        self.createCGMStructure(); 
+        self.createInsulinStructure();
+        
+        self.numDayNight = self.fixData(); # Remove to that only whole day/nigth periods are in data series.
         
     def readData(self):
         if self.readFromFile == 1: 
                     
             entries = [];
-            entries = json.load(open(self.datafile_entries))
+            entries = json.load(open(self.datafile_entries, encoding="utf8"))
 
             treatments = [];
-            treatments = json.load(open(self.datafile_treatments));
+            treatments = json.load(open(self.datafile_treatments, encoding="utf8"));
         
             self.dfEntries = pd.DataFrame.from_dict(entries);
             self.dfTreatments = pd.DataFrame.from_dict(treatments);
@@ -50,18 +55,68 @@ class Reader:
             print('Treatment size: ' + str(len(treatments)))
             print('Remove Treatments without timestamp')
             
-            idxToRemove = self.dfTreatments['timestamp'].isnull();
-            idxToKeep   = idxToRemove; 
+            self.idxToRemove = self.dfTreatments['timestamp'].isnull();
+            self.dfTreatments = self.dfTreatments[~self.idxToRemove]; # Remove all rows with timestamp = nan
+            self.dfTreatments.reset_index(inplace = True)
             
-            #for kk in range(0,len(idxToRemove)):
-            #    idxToKeep[kk] = not(idxToRemove[kk])
-                
-            #self.dfTreatments = self.dfTreatments.iloc[idxToKeep.tolist()] 
-           
+            #self.idxNanRate = self.dfTreatments['rate'].isnull(); # Det här tilldelar alla kolumne
+            #self.dfTreatments[self.idxNanRate] = 0; 
         return self.dfEntries, self.dfTreatments
-    
+     
+    def createInsulinStructure(self):     
+          self.dfInsulin = pd.DataFrame(columns=['dateTime', 'bolus', 'rate', 'deltaTimeSec'])
+          
+          self.dfInsulin['rate'] = self.dfTreatments['rate']
+          
+          " Create bolus rows for all Correction and meal boluses "
+          # TODO: Double check if this also includes the micro boluses
+          for jj in range(0,len(self.dfInsulin['dateTime'])):
+              if self.dfTreatments['eventType'][jj] == 'Correction Bolus' :
+                  self.dfInsulin['bolus'][jj] = self.dfTreatments['insulin'][jj]
+              elif self.dfTreatments['eventType'][jj] == 'Meal Bolus' :
+                  if np.isnan(self.dfTreatments['insulin'][jj]) :
+                      self.dfInsulin['bolus'][jj] = 0
+                  else:
+                      self.dfInsulin['bolus'][jj] = self.dfTreatments['insulin'][jj]
+              else: 
+                  self.dfInsulin['bolus'][jj] = 0;     
+          
+          " Calculate deltatimeSec for each row "
+          tempOld = dt.time(0);
+          for ii in range(0,len(self.dfTreatments['timestamp'])):
+            try: 
+                temp = dt.datetime.strptime(self.dfTreatments['timestamp'][ii], '%Y-%m-%dT%H:%M:%S%fZ');
+            except:
+                print(self.dfEntries['dateString'])
+                print('ii: ' + str(ii))
+                temp = 0;
+                temp = dt.datetime.strptime('2019-11-01T22:04:25.814+0100', '%Y-%m-%dT%H:%M:%S.%fZ');
+            self.dfInsulin['dateTime'][ii]  = temp;
+            if ii == 0:
+                print('hej')
+            else:
+                sec = tempOld - temp;
+                self.dfInsulin['deltaTimeSec'][ii] = sec.total_seconds();
+            tempOld = temp; 
+          
+          #self.idxRate = ~self.dfTemp.isin([0])
+          idxRateNan = self.dfInsulin['rate'].isna()
+          self.dfInsulin['rate'][idxRateNan] = 0
+           
+          idx = list();  
+          for ii in range(0,len(self.dfInsulin['rate'])):
+              if self.dfInsulin['rate'][ii] == 0:
+                  idx.append(ii);
+              else:
+                  rateNow = self.dfInsulin['rate'][ii];
+                  while len(idx) > 0: 
+                      self.dfInsulin['rate'][idx.pop()] = rateNow; 
+                  idx = list();     
+              
+          return self.dfInsulin;
+   
     def createCGMStructure(self):
-        self.dfCGM = pd.DataFrame(columns=['dateTime', 'cgm', 'deltaTime'])
+        self.dfCGM = pd.DataFrame(columns=['dateTime', 'cgm', 'deltaTimeSec'])
         self.dfCGM['cgm'] = self.dfEntries['sgv']
         
         if len(self.dfEntries['sgv']) != len(self.dfEntries['dateString']):
@@ -82,40 +137,15 @@ class Reader:
                 print('inget')
             else:
                 sec = tempOld - temp;
-                self.dfCGM['deltaTime'][ii] = sec.total_seconds();
+                self.dfCGM['deltaTimeSec'][ii] = sec.total_seconds();
             tempOld = temp; 
             
         return self.dfCGM
-    
-    def createBolusStructure(self):     
-        #Bolusself.dfb = pd.DataFrame(columns=['dateTime', 'bolus', 'basal'])
-        # dfBolus = ['dateTime', 'bolus'])
-        self.dfBolus['dateTime'] = self.dfTreatments['timestamp']
         
-        for jj in range(0,len(self.dfBolus['dateTime'])):
-            if self.dfTreatments['eventType'][jj] == 'Correction Bolus' :
-                self.dfBolus['bolus'][jj] = self.dfTreatments['insulin'][jj]
-            elif self.dfTreatments['eventType'][jj] == 'Meal Bolus' :
-                 self.dfBolus['bolus'][jj] = self.dfTreatments['insulin'][jj]
-            else: 
-                 self.dfBolus['bolus'][jj] = 0;
-         
-        np.sum(self.dfBolus['bolus'])        
-                
-        return 1;
-
-    def createBasalRateStructure(self):
-        # Hitta alla rate != 0 och != Nan
-        rateV = self.dfTreatments['rate'].tolist();
-        idxNan = np.argwhere(np.isnan(rateV)).flatten();
-        for kk in range(0,len(idxNan)):
-            rateV[idxNan[kk]] = 0.0; 
-
-        # 
-        self.dfBasal['dateTime'] = self.dfTreatments['timestamp'];
-        self.dfBasal['basalRate']     = rateV;
-        
-        return 1; 
+    def fixData(self): 
+        # Putsa till data så att det endast är hela dygn med.    
+        numDayNight = 2;  
+        return numDayNight; 
     
     def createCarbStructure(self):
         
@@ -128,16 +158,12 @@ class Reader:
         for jj in dateTemp:
             self.dfCarbs['dateTime'][counter] = dt.datetime.strptime(jj, '%Y-%m-%dT%H:%M:%SZ');
             counter = counter + 1;
-        
-
-
-        
         #tempNext = dt.datetime.strptime(self.dfTreatments['timestamp'][1], '%Y-%m-%dT%H:%M:%SZ');
         temp = dt.datetime.strptime(self.dfTreatments['timestamp'][0], '%Y-%m-%dT%H:%M:%SZ');
-        for ii in range(1,len(self.dfBasal['deltaTime'])):    
+        for ii in range(1,len(self.dfBasal['deltaTimeSec'])):    
             #tempNext = self.dfBasal['dateTime'][ii-1] - self.dfBasal['dateTime'][ii];
             sec = self.dfBasal['dateTime'][ii-1] - self.dfBasal['dateTime'][ii];
             #sec = temp - tempNext;
-            self.dfBasal['deltaTime'][ii] = sec.total_seconds();
+            self.dfBasal['deltaTimeSec'][ii] = sec.total_seconds();
             #temp = tempNext; 
             